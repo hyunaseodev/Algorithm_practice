2af90abd4910cef2d0736b313b48292838717c4e
Hyuna Seo <83485264+hyunaseodev@users.noreply.github.com>
2022-04-13 12:07:11 +0900

    Time: 15 ms (83.46%), Space: 42.5 MB (82.24%) - LeetHub

--- LeetCode/326-power-of-three/326-power-of-three.java
+++ LeetCode/326-power-of-three/326-power-of-three.java
@@ -1,14 +1,14 @@
-public class Solution {
+class Solution {
     public boolean isPowerOfThree(int n) {
-        if (n <= 0) {
-            return false;
-        }
-        while (n > 1) {
-            if (n % 3 != 0) {
-                return false;
-            }
-            n = n / 3;
-        }
-        return true;
+        if(n==0) return false;
+        if(n==1) return true;
+
+        return recur(n);
+    }
+
+    boolean recur(int r){
+        if(r == 3) return true;
+        else if((r/3)%3 != 0 || r%3 != 0) return false;
+        else return recur(r/3);
     }
 }
