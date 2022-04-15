d5320d797904caf22a453f2190d34ad5c518e3c0
Hyuna Seo <83485264+hyunaseodev@users.noreply.github.com>
2022-04-15 22:13:25 +0900

    Update ［1차］ 비밀지도.java
    비트연산 OR 활용

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -2,38 +2,18 @@ class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
 
-        String[] strArr1 = new String[n];
-        String[] strArr2 = new String[n];
-
-        //arr1,arr2 원소를 이진법으로 계산
-        //계산된 값을 String으로 변환
         for(int i=0; i<n; i++){
-            strArr1[i] = Integer.toBinaryString(arr1[i]);
-            strArr2[i] = Integer.toBinaryString(arr2[i]);
+            answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
 
-            while(n-strArr1[i].length() > 0){
-                strArr1[i] = "0" + strArr1[i];
-            }
-
-            while(n-strArr2[i].length() > 0){
-                strArr2[i] = "0" + strArr2[i];
+            while(n-answer[i].length() > 0){
+                answer[i] = "0" + answer[i];
             }
         }
 
         for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(strArr1[i].charAt(j) == '1' || strArr2[i].charAt(j) == '1') {
-                    if(j == 0) answer[i] = "#";
-                    else answer[i] += "#";
-                }
-                else {
-                    if(j == 0) answer[i] = " ";
-                    else answer[i] += " ";
-                }
-            }
+            answer[i] = answer[i].replace("1", "#");
+            answer[i] = answer[i].replace("0", " ");
         }
-
-        //System.out.println(Arrays.toString(answer));
 
         return answer;
     }
