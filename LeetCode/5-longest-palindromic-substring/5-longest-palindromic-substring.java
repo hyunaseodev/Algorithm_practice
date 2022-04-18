bdd54bbca865f9410602004d4ee691cc76fb6cdc
Hyuna Seo <83485264+hyunaseodev@users.noreply.github.com>
2022-04-18 11:49:19 +0900

    Time: 40 ms (71.64%), Space: 42.4 MB (89.68%) - LeetHub

---
+++ LeetCode/5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        if(s == null || s.length() < 1) return "";
+
+        String answer = "";
+
+        for(int i=0; i<s.length(); i++){
+            int len1 = expandFromMiddle(s, i, i);
+            int len2 = expandFromMiddle(s, i, i+1);
+            int len = Math.max(len1, len2);
+            if(answer.length() < len) {
+                answer = s.substring(i-(len-1)/2, i+len/2+1);
+            }
+        }
+
+        return answer;
+    }
+    
+    public int expandFromMiddle(String s, int left, int right) {
+        if(s == null || left > right) return 0;
+
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }   
+}
