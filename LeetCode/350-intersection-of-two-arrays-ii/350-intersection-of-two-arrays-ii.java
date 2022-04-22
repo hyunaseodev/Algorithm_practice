49c932e359c56985c3f74d3708a937fb73644405
Hyuna Seo <83485264+hyunaseodev@users.noreply.github.com>
2022-04-22 11:45:18 +0900

    Time: 2 ms (95.23%), Space: 42.5 MB (83.76%) - LeetHub

---
+++ LeetCode/350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        int[] answerArr = new int[Math.max(nums1.length, nums2.length)];
+
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        int nums1Pointer = 0;
+        int nums2Pointer = 0;
+        int cnt = 0;
+
+        while (nums1Pointer < nums1.length && nums2Pointer < nums2.length) {
+            if (nums1[nums1Pointer] > nums2[nums2Pointer])
+                nums2Pointer++;
+            else if (nums1[nums1Pointer] < nums2[nums2Pointer])
+                nums1Pointer++;
+            else if (nums1[nums1Pointer] == nums2[nums2Pointer]) {
+                answerArr[cnt] = nums1[nums1Pointer];
+                nums1Pointer++;
+                nums2Pointer++;
+                cnt++;
+            }
+        }
+
+        return Arrays.copyOfRange(answerArr, 0, cnt);
+    }
+}
