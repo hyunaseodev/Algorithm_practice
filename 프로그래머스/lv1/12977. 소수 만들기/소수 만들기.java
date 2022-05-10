fe0fba6f867cd7c6ab52ad62eb1007d7cc249f4b
Hyuna Seo <83485264+hyunaseodev@users.noreply.github.com>
2022-05-10 13:43:23 +0900

    [level 1] Title: 소수 만들기, Time: 1.67 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -1,32 +1,27 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(int[] nums) {
-		List<Integer> sum = new ArrayList<>();
-		
-		//배열원소 3개씩 더한 값을 list에 담기
-		for(int i = 0; i < nums.length; i++) {
-			for(int j = i+1; j < nums.length; j++) {
-				for(int z = j+1; z < nums.length; z++) {
-					sum.add(nums[i] + nums[j] + nums[z]);
-				}
-			}
-		}
-		
-		//list의 각 원소가 소수인지 확인
-		//소수가 아니면 삭제
-		int answer = 0;
-		for(int i = 0; i < sum.size(); i++) {
-			System.out.println(sum.get(i));
-			int div_cnt = 0;
-			for(int j = 2; j < sum.get(i); j++) {
-				if(sum.get(i) % j == 0) div_cnt += 1;
-				if(div_cnt > 1) break;
-			}
-			
-			if(div_cnt == 0) {
-				answer += 1;
-			}
-		}
-        return answer;
+        List<Integer> sum = new ArrayList<>();
+
+        //배열원소 3개씩 더한 값을 list에 담기
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i+1; j < nums.length; j++) {
+                for(int z = j+1; z < nums.length; z++) {
+                    sum.add(nums[i] + nums[j] + nums[z]);
+                }
+            }
+        }
+
+        return (int)sum.stream()
+                        .filter(i -> isPrime(i))
+                        .count();
+    }
+
+    boolean isPrime(int n){
+        return IntStream.range(2, n)
+                        .allMatch(i -> n % i != 0);
     }
 }
